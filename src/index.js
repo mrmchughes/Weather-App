21c41e60188437e0e34296074664b545ee6a967d
mrmchughes <mrmchughes27@gmail.com>
2021-10-14 14:15:23 -0700

    pseduo code for needed functions

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// Console log test works, everything set up correctly
+console.log("testing!");
+
+// Function to search Weather API using location
+function weatherSearch() {
+  // Take a location
+  // Access Weather API using the location
+}
+
+// Function to retrieve weather data for submitted location
+function returnWeatherData() {
+  // Return weather JSON data for location from weatherSearch()
+}
+
+// Toggle display data in Fahrenheit or Celsius
+function toggleDisplayTemp() {
+  // Toggle temperature between Fahrenheit or Celsius
+}
+
+// Function to process JSON data from the API
+function weatherJSONDataProcess() {
+  // Process JSON data from returnWeatherData()
+  // Possibly JSON.stringify()?
+}
+
+// Function to return object with only needed data for app
+function weatherDataObject() {
+  // Return object with only the required JSON data for app
+}
+
+// Form that allows users to input their location, and fetch weather info
+function formSubmitWeather() {
+  // Simple Form, user can input location
+  // Runs appropriate above functions with user location
+}
+
+// Optional loading component that displays while data is being retrieved
+function loadingDisplay() {
+  // displays loading on screen until info comes back from API
+}
+
+// Change the look of the page based on the data from Weather API
+//      Use standard Weather Icons, gifs could look less professional
+
+// Incorporate Async/Await into the code
