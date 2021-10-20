6f85f5ba6bfcec8c0fe4e55503742dbdeae92382
mrmchughes <mrmchughes27@gmail.com>
2021-10-20 14:42:37 -0700

    css added, catch added, done for now

--- src/index.js
+++ src/index.js
@@ -8,110 +8,63 @@ const windSpeed = document.getElementById("windSpeed");
 const humidity = document.getElementById("humidity");
 const pressure = document.getElementById("pressure");
 
-// Function to search Weather API using location
-async function weatherSearchFahrenheit(e) {
-  // Take a submitted location
-  e.preventDefault();
-  console.log("New weather search!");
-  const searchTerm = document.querySelector("#weatherSearchInput").value;
-  console.log("This is the current value of search term: " + searchTerm);
-
+// Function to search Weather API using location, returns data in Fahrenheit
+async function weatherSearchFahrenheit() {
   try {
     const response = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
-        searchTerm +
+        document.querySelector("#weatherSearchInput").value +
         "&units=imperial&appid=e1e165ab654f897773dcd2276f118a81",
       { mode: "cors" }
     );
     const returnedWeather = await response.json();
 
-    console.log(returnedWeather);
-    // Consider rewording
-
-    cityName.innerHTML = returnedWeather.name;
-    weatherType.innerHTML = returnedWeather.weather[0].description + ".";
-    temperature.innerHTML = returnedWeather.main.temp + " degrees Fahrenheit.";
-    tempFeels.innerHTML =
-      returnedWeather.main.feels_like + " degrees Fahrenheit.";
-    windSpeed.innerHTML = returnedWeather.wind.speed + " mph.";
+    cityName.innerHTML = returnedWeather.name.toUpperCase();
+    weatherType.innerHTML =
+      returnedWeather.weather[0].description.toUpperCase();
+    temperature.innerHTML = returnedWeather.main.temp + " &deg;F";
+    tempFeels.innerHTML = returnedWeather.main.feels_like + " &deg;F";
+    windSpeed.innerHTML = returnedWeather.wind.speed + " mph";
     humidity.innerHTML = returnedWeather.main.humidity + "%";
     pressure.innerHTML = returnedWeather.main.pressure;
+
+    document.querySelector("#weatherSearchInput").value = null;
   } catch (error) {
-    console.log(error);
+    alert("Sorry, could not find that city.");
   }
 }
 
-async function weatherSearchCelsius(e) {
-  // Take a submitted location
-  e.preventDefault();
-  console.log("New weather search!");
-  const searchTerm = document.querySelector("#weatherSearchInput").value;
-  console.log("This is the current value of search term: " + searchTerm);
-
+// Function to search Weather API using location, returns data in Celsius
+async function weatherSearchCelsius() {
   try {
     const response = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
-        searchTerm +
+        document.querySelector("#weatherSearchInput").value +
         "&units=metric&appid=e1e165ab654f897773dcd2276f118a81",
       { mode: "cors" }
     );
     const returnedWeather = await response.json();
-    console.log(returnedWeather);
-    // Consider rewording
 
-    cityName.innerHTML = returnedWeather.name;
-    weatherType.innerHTML = returnedWeather.weather[0].description + ".";
-    temperature.innerHTML = returnedWeather.main.temp + " degrees Celsius.";
-    tempFeels.innerHTML = returnedWeather.main.feels_like + " degrees Celsius.";
-    windSpeed.innerHTML = returnedWeather.wind.speed + " mph.";
+    cityName.innerHTML = returnedWeather.name.toUpperCase();
+    weatherType.innerHTML =
+      returnedWeather.weather[0].description.toUpperCase();
+    temperature.innerHTML = returnedWeather.main.temp + " &deg;C";
+    tempFeels.innerHTML = returnedWeather.main.feels_like + " &deg;C";
+    windSpeed.innerHTML = returnedWeather.wind.speed + " mps";
     humidity.innerHTML = returnedWeather.main.humidity + "%";
     pressure.innerHTML = returnedWeather.main.pressure;
+
+    document.querySelector("#weatherSearchInput").value = null;
   } catch (error) {
-    console.log(error);
+    alert("Sorry, could not find that city.");
   }
 }
-
-// Need a catch if the city entered does not exsist
 
 const fahrenheitButton = document.getElementById("fahrenheitButton");
 fahrenheitButton.addEventListener("click", weatherSearchFahrenheit);
 
-const celciusButton = document.getElementById("celciusButton");
-celciusButton.addEventListener("click", weatherSearchCelsius);
-
-// Function to retrieve weather data for submitted location
-function returnWeatherData() {
-  // Return weather JSON data for location from weatherSearch()
-}
-
-// Function to process JSON data from the API
-function weatherJSONDataProcess() {
-  // Process JSON data from returnWeatherData()
-  // Possibly JSON.stringify()?
-}
-
-// Function to return object with only needed data for app
-function weatherDataObject() {
-  // Return object with only the required JSON data for app
-}
-
-// Form that allows users to input their location, and fetch weather info
-function formSubmitWeather() {
-  // Simple Form, user can input location
-  // Runs appropriate above functions with user location
-}
-
-// Toggle display data in Fahrenheit or Celsius
-function toggleDisplayTemp() {
-  // Toggle temperature between Fahrenheit or Celsius
-}
-
-// Optional loading component that displays while data is being retrieved
-function loadingDisplay() {
-  // displays loading on screen until info comes back from API
-}
+const celsiusButton = document.getElementById("celsiusButton");
+celsiusButton.addEventListener("click", weatherSearchCelsius);
 
 // Change the look of the page based on the data from Weather API
-//      Use standard Weather Icons, gifs could look less professional
-
-// Incorporate Async/Await into the code
+//      Use openWeather Icons, gifs could look less professional
